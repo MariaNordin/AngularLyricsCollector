1dabcc6eca68b35e0b1b63abdbceabbd00cfbc5d
Maria Nordin <miamarianordin@gmail.com>
2021-08-31 08:35:51 +0200

    Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './pages/search/search.component';
@@ -20,6 +21,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MDBBootstrapModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
